449699fca39e356787da846314201122efa14073
Binnn05 <166401783+Binnn05@users.noreply.github.com>
2025-04-27 20:17:31 +0700

    Add files via upload

---
+++ AbyanKing_122140063_pertemuan4/AbyanKing_122140063_pertemuan4/math_operations.py
@@ -0,0 +1,29 @@
+# math_operations.py
+
+PI = 3.14159
+
+# Fungsi Geometri
+def luas_persegi(sisi):
+    return sisi * sisi
+
+def keliling_persegi(sisi):
+    return 4 * sisi
+
+def luas_persegi_panjang(panjang, lebar):
+    return panjang * lebar
+
+def keliling_persegi_panjang(panjang, lebar):
+    return 2 * (panjang + lebar)
+
+def luas_lingkaran(radius):
+    return PI * radius * radius
+
+def keliling_lingkaran(radius):
+    return 2 * PI * radius
+
+# Fungsi Konversi Suhu
+def celsius_ke_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def celsius_ke_kelvin(c):
+    return c + 273.15
